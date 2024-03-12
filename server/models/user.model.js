7b25c1fc378f6b14e76d8e46ef93b451ed2fef41
ahmad <ahmad@io.com>
2024-03-12 14:37:08 +0200

    create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,24 @@
+import moongoose from "mongoose";
+const UserSchema = moongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = moongoose.model("User", UserSchema);
+export default User;
